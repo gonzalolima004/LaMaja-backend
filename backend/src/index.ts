4dce4a1cd6adf08e94840ddd8a93cee462daf6d4
Mauricio_Moll12 <mauricio_moll321@hotmail.com>
2025-09-18 23:40:39 -0300

    usuario controller y routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,19 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
 import cors from 'cors';
+import usuarioRoutes from './routes/usuarioRoutes'; 
 
+dotenv.config();
 
 export const prisma = new PrismaClient();
 
-dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-
+// Rutas
+app.use("/api/usuarios", usuarioRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Backend corriendo en puerto ${PORT}`));
